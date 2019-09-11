fea7df57aaf674748429430229e754ca2b11153f
matinator <50795539+matinator@users.noreply.github.com>
2019-09-11 19:33:17 +0200

    function to add column to dataframe
    At the moment there is only one function which assign number as category in equal portion of the dataframe

--- Add_category.py
+++ Add_category.py
@@ -4,7 +4,7 @@ import numpy as np
 def add_category(frame, n_category):
     categories = np.zeros(len(frame),dtype=int)
     values  = np.array(range(n_category),dtype=int)
-    #replace values in category from 0
+    #replace values in category from 1
     for ind,value in enumerate(values,1):
         start = int(len(frame)*(ind-1)/n_category)
         end = int((len(frame)*ind/n_category))
