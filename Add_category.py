f1151fd19f01d964d3746b1b5979b86f6cbe1e7b
matinator <50795539+matinator@users.noreply.github.com>
2019-09-11 19:23:32 +0200

    Add files via upload
    function to add column to the dataframe

---
+++ Add_category.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+def add_category(frame, n_category):
+    categories = np.zeros(len(frame),dtype=int)
+    values  = np.array(range(n_category),dtype=int)
+    #replace values in category from 0
+    for ind,value in enumerate(values,1):
+        start = int(len(frame)*(ind-1)/n_category)
+        end = int((len(frame)*ind/n_category))
+        categories[start:end] = value
+    #add column category
+    frame['category'] = categories
+    
