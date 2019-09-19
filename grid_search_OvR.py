52c0e7c16687cc99a3b2771f8609a9bd82e8b7bd
matinator <matteoconterno97@gmail.com>
2019-09-19 12:11:37 +0200

    grid search with results saved as dataframe and plot of index-roc_auc

--- grid_search_OvR.py
+++ grid_search_OvR.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 import argparse
+import matplotlib.pyplot as plt
 from sklearn.multiclass import OneVsRestClassifier
 from xgboost import XGBClassifier
 from sklearn.model_selection import KFold
@@ -50,17 +51,20 @@ Y_df = training_df.filter(like='category', axis=1)
 max_depth=[2,3,5,8]
 n_estimators=[100,200,500]
 learning_rate=[0.1,0.2,0.3]
+
 #all combination of parameter
 parameters = product(max_depth,n_estimators,learning_rate)
 
 #folding for cross validation
 kf = KFold(n_splits=5, shuffle= True, random_state=42)
-#create files for storing scores
-f = open('roac_auc_score.txt','w+')
-f.write('ROC_AUC_SCORES \n')
+
+#list for dataframe of results and parameters
+list_for_df = []
+
+print('grid search started')
 
 #manual grid search
-for param in parameters:
+for ipar,param in enumerate(parameters):
     for index in kf.split(training_df):
         # onevsrest classifier
         clf = OneVsRestClassifier(XGBClassifier(n_jobs=-1, max_depth=param[0],
@@ -80,7 +84,17 @@ for param in parameters:
             # Compute micro-average ROC curve and ROC area
             fpr["micro"], tpr["micro"], _ = roc_curve(y_test.loc[:, 'category_0':'category_{0}'.format(n_classes)].values.ravel(),y_score.ravel())
             roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-        f.write('max_depth = {0}, n_estimator = {1}, learning_rate = {2}, roc_auc_micro_score = {3} \n'.format(
-            param[0],param[1],param[2],roc_auc['micro']))
+        #temporary list
+        tmp_list = [ipar,param[0],param[1], param[2], roc_auc['micro']]
+        list_for_df.append(tmp_list)
 
-
+#list of columns for df
+columns_df = ['index','n_estimator','max_depth','learning_rate','roc_auc_micro']
+#dataframe of the results of grid search
+df_results = pd.DataFrame(list_for_df,columns = columns_df)
+#conversion to parquet
+df_results.to_parquet('results_grid_search.parquet.gzip',compression='gzip')
+print('dataframe of the results of grid search saved')
+#plotting datas
+df_results.plot(x='index', y='roc_auc_micro', kind ='line')
+plt.show()
