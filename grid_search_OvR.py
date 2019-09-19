c1593e93b2bd9635c805a886cdbdcef604792ec7
matinator <matteoconterno97@gmail.com>
2019-09-19 15:46:50 +0200

    final functional version of grid search of OnevsRestClassifier with XGBoost

--- grid_search_OvR.py
+++ grid_search_OvR.py
@@ -7,14 +7,14 @@ import matplotlib.pyplot as plt
 from sklearn.multiclass import OneVsRestClassifier
 from xgboost import XGBClassifier
 from sklearn.model_selection import KFold
-from sklearn.metrics import roc_curve,auc
+from sklearn.metrics import roc_curve, auc
 from Add_category import multi_column
 from itertools import product
 
 # flags
 PARSER = argparse.ArgumentParser(description='Arguments to pass')
 PARSER.add_argument('dir', metavar='text', default='.',
-    help='flag to search in data directory, remember put all paths!')
+                    help='flag to search in data directory, remember put all paths!')
 ARGS = PARSER.parse_args()
 
 # files
@@ -35,92 +35,108 @@ for keys in data:
 
 print('adding category to dataframes')
 
-#add category to dataframes
+# add category to dataframes
 multi_column(data)
 
-#training columns
-training_columns = ['p','pTPC','ITSclsMap','dEdxITS','NclusterPIDTPC','dEdxTPC']
+# training columns
+training_columns = ['p', 'pTPC', 'ITSclsMap',
+                    'dEdxITS', 'NclusterPIDTPC', 'dEdxTPC']
 
-#training dataframe
-training_df = pd.concat([data[keys].iloc[:5000] for keys in data],ignore_index = True)
+# training dataframe
+training_df = pd.concat([data[keys].iloc[:5000]
+                         for keys in data], ignore_index=True)
 
-#traing data with training columns
+# traing data with training columns
 X_df = training_df[training_columns]
 Y_df = training_df.filter(like='category', axis=1)
 
-#parameters for grid search
-max_depth=[2,3,5,8]
-n_estimators=[100,200,500]
-learning_rate=[0.1,0.2,0.3]
+# parameters for grid search
+max_depth = [2, 3]  # ,3,5,8]
+n_estimators = [100]  # ,200,500]
+learning_rate = [0.1]  # ,0.2,0.3]
 
-#all combination of parameter
-parameters = product(max_depth,n_estimators,learning_rate)
+# all combination of parameter
+parameters = product(max_depth, n_estimators, learning_rate)
 
-#number of folds
+# number of folds
 n_folds = 5
 
-#folding for cross validation
-kf = KFold(n_splits=n_folds, shuffle= True, random_state=42)
+# folding for cross validation
+kf = KFold(n_splits=n_folds, shuffle=True, random_state=42)
 
-#list for dataframe of results and parameters
+# list for dataframe of results and parameters
 list_for_df = []
 
 print('grid search started')
 
-#manual grid search
-for ipar,param in enumerate(parameters):
+# manual grid search
+for ipar, param in enumerate(parameters):
     for index in kf.split(training_df):
         # onevsrest classifier
         clf = OneVsRestClassifier(XGBClassifier(n_jobs=-1, max_depth=param[0],
-         n_estimators=param[1],learning_rate=param[2]))
+                                                n_estimators=param[1], learning_rate=param[2]))
         x_train = X_df.iloc[index[0]]
         y_train = Y_df.iloc[index[0]]
-        x_test  = X_df.iloc[index[1]]
-        y_test  = Y_df.iloc[index[1]]
-        y_score = clf.fit(x_train,y_train).predict_proba(x_test)
-        n_classes= range(len(data.keys()))
-        fpr=dict()
-        tpr=dict()
-        roc_auc=dict()
+        x_test = X_df.iloc[index[1]]
+        y_test = Y_df.iloc[index[1]]
+        y_score = clf.fit(x_train, y_train).predict_proba(x_test)
+        n_classes = range(len(data.keys()))
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
         for cat in n_classes:
-            fpr[cat], tpr[cat], _ = roc_curve(y_test.loc[:, 'category_{0}'.format(cat)].values, y_score[:, cat])
-            roc_auc[cat] = auc(fpr[cat], tpr[cat]) 
+            fpr[cat], tpr[cat], _ = roc_curve(
+                y_test.loc[:, 'category_{0}'.format(cat)].values, y_score[:, cat])
+            roc_auc[cat] = auc(fpr[cat], tpr[cat])
             # Compute micro-average ROC curve and ROC area
-            fpr["micro"], tpr["micro"], _ = roc_curve(y_test.loc[:, 'category_0':'category_{0}'.format(n_classes)].values.ravel(),y_score.ravel())
+            fpr["micro"], tpr["micro"], _ = roc_curve(
+                y_test.loc[:, 'category_0':'category_{0}'.format(n_classes)].values.ravel(), y_score.ravel())
             roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-        #temporary list
-        tmp_list = [ipar,param[0],param[1], param[2], roc_auc['micro']]
+        # temporary list
+        tmp_list = [ipar, param[0], param[1], param[2], roc_auc['micro']]
         list_for_df.append(tmp_list)
 
-#list of columns for df
-columns_df = ['index','n_estimator','max_depth','learning_rate','roc_auc_micro']
-#dataframe of the results of grid search
-df_results = pd.DataFrame(list_for_df,columns = columns_df)
-#number of combination of parameters
-n_set_params= len(df_results)/n_folds
-#list of average and rms
-mean_rms = []
-#new column for root_mean_square with value 0
+# list of columns for df
+columns_df = ['index', 'n_estimator',
+              'max_depth', 'learning_rate', 'roc_auc_micro']
+# dataframe of the results of grid search
+df_results = pd.DataFrame(list_for_df, columns=columns_df)
+# number of combination of parameters
+n_set_params = int(len(df_results)/n_folds)
+#new columns for dataframe
 df_results['root_mean_square'] = 0
+df_results['mean_roc_auc'] = 0
 
-#calculation of average and 
+# calculation of average and
 for ind_df in range(n_set_params):
+    #indexes
     start_ind = ind_df*n_folds
     end_ind = (ind_df+1)*n_folds
-    average_roc = df_results.loc[start_ind:end_ind,'roc_auc_micro'].mean()
-    #calculate rms mean
-    df_results.loc[start_ind:end_ind,'root_mean_square'] = sqrt((
-        df_results.loc[start_ind:end_ind,'roc_auc_micro'] - average_roc)**2).mean()
+    #average of roc_auc_micro
+    average_roc = df_results.loc[start_ind:end_ind, 'roc_auc_micro'].mean()
+    df_results.loc[start_ind:end_ind, 'mean_roc_auc'] = average_roc
 
-#conversion to parquet
-df_results.to_parquet('results_grid_search.parquet.gzip',compression='gzip')
+    # calculate rms mean
+    df_results['residuals'] = (
+        df_results.loc[start_ind:end_ind, 'roc_auc_micro'] - average_roc)**2
+    rms = sqrt(df_results.loc[start_ind:end_ind, 'residuals'].mean())
+    df_results.loc[start_ind:end_ind, 'root_mean_square'] = rms
+
+#eliminate the column residuals which is now unneccesary
+df_results = df_results.drop(columns='residuals')
+
+# conversion to parquet
+df_results.to_parquet('results_grid_search.parquet.gzip', compression='gzip')
 print('dataframe of the results of grid search saved')
 
-#plotting datas
-plt.fill_between(df_results['index'],df_results['roc_auc_micro']-df_results['root_mean_square'],
-    df_results['roc_auc_micro']+df_results['root_mean_square'])
-plt.title('roc_auc_micro - index')
+# plotting datas of average_roc_auc_micro in function of index
+plt.plot(df_results['index'], df_results['mean_roc_auc'])
+plt.fill_between(df_results['index'], df_results['mean_roc_auc']-df_results['root_mean_square'],
+                 df_results['mean_roc_auc']+df_results['root_mean_square'], alpha=0.3)
+plt.title('mean_roc_auc - index')
 plt.xlabel('index')
-plt.ylabel('roc_auc_micro')
-plt.savefig('results.pdf')
+plt.ylabel('mean_roc_auc')
+#save file
+plt.savefig('gridsearch_OvsR_results.pdf')
+#last line of finish
 print('all done')
